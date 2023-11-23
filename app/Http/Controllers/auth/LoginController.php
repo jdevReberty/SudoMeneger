ad2990bfb7789b2e682774dae56611ffca162dfb
jdevReberty <jdev.contato@gmail.com>
2023-11-23 19:42:30 -0300

    tinha esquecido de colocar o redirect do logout para o index de login

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -42,6 +42,7 @@ class LoginController extends Controller
 
     public function logout() {
         Auth::logout();
+        return redirect()->route('login');
     }
 
     public function home() {
