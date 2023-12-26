463d843725ea66fcf4574558d83f3625b383991b
jdevReberty <jdev.contato@gmail.com>
2023-12-26 15:12:45 -0300

    reorganizacao da rota de registro e criacao do grupo de rotas de empresas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Enums\EmpresaTipoVinculo;
 use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\EmpresaController;
+use App\Models\Contato;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +16,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get("teste", function() {
+    dd(Contato::first()->tipo_contato);
+});
 /* ----------------- rotas para cadastro de usuário e login ----------------- */
 Route::middleware(['out'])->group(function () {
     Route::prefix('/login')->group(function() {
         Route::get('/', [LoginController::class, 'index'])->name('login');
         Route::post('/access', [LoginController::class, 'authenticate'])->name('login.auth');
+    });
+    Route::prefix('/register')->group(function() {
+        Route::get('/', [LoginController::class, 'register'])->name('register');
+        Route::post('/save', [LoginController::class, 'newUser'])->name('register.store');
     });
 });
 
@@ -25,5 +36,10 @@ Route::middleware(['out'])->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [LoginController::class, 'home'])->name('home');
     Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::prefix('/empresa')->group(function() {
+        Route::get('/', [EmpresaController::class, 'index'])->name('empresa.index');
+        Route::match(['get', 'post'], '/create', [EmpresaController::class, 'create'])->name('empresa.create');
+        Route::post('/store', [EmpresaController::class, 'store'])->name('empresa.store');
+    });
 });
 
