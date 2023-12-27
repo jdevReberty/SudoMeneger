09dc2cdd038392c42a12ea90977d15e4ff4156b4
jdevReberty <jdev.contato@gmail.com>
2023-12-26 23:01:22 -0300

    rotas de resete password e envio de email

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Enums\EmpresaTipoVinculo;
 use App\Http\Controllers\auth\LoginController;
 use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\Mail\MailController;
+use App\Mail\DefaultPassword;
 use App\Models\Contato;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +20,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("teste", function() {
-    dd(Contato::first()->tipo_contato);
-});
+// Route::get("teste", function() {
+//     Mail::to("jdev.contato@gmail.com", "johnny")
+//     ->send(New DefaultPassword(
+//             [
+//                 'password' => "teste", 
+//                 'from' => "jdev.contato@gmail.com",
+//                 'user_id' => 34,
+//             ]
+//         )
+//     );
+// });
 /* ----------------- rotas para cadastro de usuário e login ----------------- */
 Route::middleware(['out'])->group(function () {
     Route::prefix('/login')->group(function() {
@@ -30,7 +41,22 @@ Route::middleware(['out'])->group(function () {
         Route::get('/', [LoginController::class, 'register'])->name('register');
         Route::post('/save', [LoginController::class, 'newUser'])->name('register.store');
     });
+
+    Route::prefix('/password')->group(function() {
+        Route::prefix('/default')->group(function() {
+            Route::get('/resete_default_password/{user}', [LoginController::class, 'resete_default_password'])->name('password.resete_default_password');
+            Route::post('/resete/{user}', [LoginController::class, 'new_password'])->name('password.resete');
+        });
+        Route::match(['get', 'post'], '/resete', [LoginController::class, 'resete_password'])->name('password.send_mail');
+        Route::get('/mail/resete/{user}', [LoginController::class, 'password_edit'])->name('password.edit');
+        Route::post('/new/password/{user}', [LoginController::class, 'new_password'])->name('password.update');
+    });
 });
+
+Route::prefix('/mail')->group(function() {
+    Route::get('/send_default_password', [MailController::class, 'send_default_password'])->name('mail.send_default_password');
+});
+
 
 /* ------------------------ rotas internas do sistema ----------------------- */
 Route::middleware(['auth'])->group(function () {
