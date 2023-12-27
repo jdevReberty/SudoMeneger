46f9f9c655c1a59ae6789714e5ce04de617fbae9
jdevReberty <jdev.contato@gmail.com>
2023-12-27 18:04:05 -0300

    remocao da barra de pesquisa

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
     </button>
 
     <!-- Topbar Search -->
-    <form
+    {{-- <form
         class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">
             <input type="text" class="form-control bg-light border-0 small" placeholder="Procurando por..."
@@ -18,7 +18,7 @@
                 </button>
             </div>
         </div>
-    </form>
+    </form> --}}
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
@@ -61,7 +61,6 @@
                 } else {
                     $qtdPendentes = 0;
                 }
-                // dd($qtdPendentes);
             @endphp
             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
