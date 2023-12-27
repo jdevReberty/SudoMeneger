bc9902018173f6e2a12b63da0304f5140c60a9ea
jdevReberty <jdev.contato@gmail.com>
2023-12-27 12:19:17 -0300

    adicao dos metodos de cadastro de endereco e contato

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\EmpresaTipoVinculo;
 use App\Enums\UsuarioEmpresaStatus;
 use App\Http\Requests\CreateEmpresaRequest;
-use App\Models\Empresa;
-use App\Models\UsuarioEmpresa;
+use App\Models\{Contato, Empresa, Endereco, UsuarioEmpresa};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -15,11 +14,14 @@ class EmpresaController extends Controller
 {
     public function __construct(
         protected Empresa $empresa,
+        protected Contato $contato,
+        protected Endereco $endereco,
         protected UsuarioEmpresa $usuarioEmpresa,
     ){}
 
-    public function index() {
-
+    public function index(Empresa $empresa) {
+        $empresa->with(['contato', 'endereco']);
+        return view('pages.empresa.index', compact('empresa'));
     }
 
     public function create(Request $request) {
@@ -55,6 +57,36 @@ class EmpresaController extends Controller
         } catch (\Throwable $th) {
             DB::rollBack();
             return redirect()->back()->withErrors(["errors" => $th->getMessage()])->withInput();
+        }
+    }
+
+    public function create_contato(Empresa $empresa) {
+        return view('pages.contato.empresa.create', compact('empresa'));
+    }
+
+    public function store_contato(Request $request, Empresa $empresa) {
+        try {
+            $request['id_empresa'] = $empresa->id;
+            $request['tipo_contato'] = 'profissional';
+            $this->contato->create($request->toArray());
+            return redirect()->route('empresa.index', ['empresa' => $empresa->id]);
+        } catch (\Throwable $th) {
+            return redirect()->back()->withErrors(['errors' => $th->getMessage()]);
+        }
+    }
+
+    public function create_endereco(Empresa $empresa) {
+        return view('pages.endereco.empresa.create', compact('empresa'));
+    }
+
+    public function store_endereco(Request $request, Empresa $empresa) {
+        try {
+            $request['id_empresa'] = $empresa->id;
+            $request['tipo_endereco'] = 'profissional';
+            $this->endereco->create($request->toArray());
+            return redirect()->route('empresa.index', ['empresa' => $empresa->id]);
+        } catch (\Throwable $th) {
+            return redirect()->back()->withErrors(['errors' => $th->getMessage()]);
         }
     }
 }
